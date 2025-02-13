9344bdb91c678468c000704880d2e6ce347be808
ali01b <kaymak210621@gmail.com>
2025-02-14 02:36:32 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Google Chrome'u yükle
+# Google Chrome'u indir ve yükle
 echo "Google Chrome yükleniyor..."
 curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
+apt update && apt install -y ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
-# ChromeDriver'ı yükle
+# ChromeDriver'ı indir ve yükle
 echo "ChromeDriver yükleniyor..."
 CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
-mv chromedriver /usr/bin/chromedriver
+mv chromedriver /opt/chromedriver  # ChromeDriver'ı /opt dizinine taşıyoruz
 rm chromedriver_linux64.zip
 
 # Python bağımlılıklarını yükle
